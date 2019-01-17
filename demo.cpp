8b3cd8d1ccd5a57dbab65e7ba53ace44b3a34786
lokeshIIITB <lglg798@gmail.com>
2019-01-17 09:58:48 +0530

    cpp program

---
+++ demo.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	cout<<"Enter any two number\n";
+	int a,b;
+	cin>>a>>b;
+	cout<<"addition of two numbers = "<<a+b<<endl;
+	cout<<"multiplucation of two numbers  = "a*b<<endl;
+	return 0;
+}
